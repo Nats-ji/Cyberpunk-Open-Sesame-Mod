ea7fefc1e7c3d4e2c854bd6be83f882fa10d0b30
Mingming Cui <Angel_tommy@foxmail.com>
2021-01-09 00:28:14 +0800

    Added Vehicle support, fixed timer

--- open_sesame/init.lua
+++ open_sesame/init.lua
@@ -1,34 +1,55 @@
 registerForEvent("onInit", function()
 	HotKey = 0x45 -- Change Hotkey Here. You can find Key Codes at https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-	ts=Game.GetTimeSystem()
     drawFakeDoor = false
-    trapInput = false
-	gameTime = 0
+	drawDoor = false
+	drawVehicle = false
+	getTime = 0
     print("Open Semame Mod Loaded... Press E to open any door")
 end)
 
 registerForEvent("onUpdate", function()
 		if (ImGui.IsKeyPressed(HotKey, false)) then
 			objLook = Game.GetTargetingSystem():GetLookAtObject(Game.GetPlayer(),false,false)
-			dsname = objLook:GetDisplayName()
-			if (dsname == "LocKey#69") or (dsname == "LocKey#74") or (dsname == "LocKey#2136") or (dsname == "Gameplay-Devices-DisplayNames-DoorLift") or (dsname == "Gameplay-Devices-DisplayNames-Door") then
-				if (objLook.OpenDoor) then
-					objLook:OpenDoor() -- Real Door
-				else
-					gameTime = ts:GetGameTimeStamp() -- Use Time Stemp from the game to calcuate tooltip appear time.
-					drawFakeDoor = true -- Fake Door
-				end
+			objType = objLook:ToString()
+			if (objType == "Door") then
+				objLook:OpenDoor() -- Real Door
+				getTime = os:clock()
+				drawDoor = true
+			elseif (objType == "FakeDoor") then
+				getTime = os:clock()
+				drawFakeDoor = true -- Fake Door
+			elseif (objLook:IsVehicle()) then
+				vehName = objLook:GetDisplayName()
+				vehPS = objLook:GetVehiclePS()
+				vehPS:UnlockAllVehDoors()	-- Open Vehicle Doors
+				getTime = os:clock()
+				drawVehicle = true
 			end
 		end
 end)
 
 registerForEvent("onDraw", function()
 	if (drawFakeDoor) then
-	ImGui.BeginTooltip()
-		ImGui.Text("This is a fake door...")
-	ImGui.EndTooltip()
-	end
-	if (ts:GetGameTimeStamp() > gameTime + 8) then
-		drawFakeDoor = false
+		ImGui.BeginTooltip()
+			ImGui.Text("This is a fake door...")
+		ImGui.EndTooltip()
+		if (os:clock() > getTime + 0.8) then
+			drawFakeDoor = false
+		end
+	elseif (drawDoor) then
+		ImGui.BeginTooltip()
+			ImGui.Text("OPEN SESAME!")
+		ImGui.EndTooltip()
+		if (os:clock() > getTime + 0.8) then
+			drawDoor = false
+		end
+	elseif (drawVehicle) then
+		ImGui.BeginTooltip()
+			vehToolTip = vehName.." has been unlocked!"
+			ImGui.Text(vehToolTip)
+		ImGui.EndTooltip()
+		if (os:clock() > getTime + 1) then
+			drawVehicle = false
+		end
 	end
 end)
