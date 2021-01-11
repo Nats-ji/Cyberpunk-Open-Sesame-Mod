49cda852bacf14b2800087d5de32834af2185e80
Mingming Cui <Angel_tommy@foxmail.com>
2021-01-11 23:19:27 +0800

    Update init.lua

--- open_sesame/init.lua
+++ open_sesame/init.lua
@@ -1,3 +1,16 @@
+-- Copyright 2020-2021 Mingming Cui
+-- This file is part of Cyberpunk 2077 Open Sesame Mod.
+-- Cyberpunk 2077 Open Sesame Mod is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- Cyberpunk 2077 Open Sesame Mod is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- You should have received a copy of the GNU General Public License
+-- along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
 registerForEvent("onInit", function()
 	HotKey = 0x45 -- Change Hotkey Here. You can find Key Codes at https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 	drawPopup = false
