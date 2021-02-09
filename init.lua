a3cc73d3a4712d76f8ee9790d92b5677b05241f6
Nats-ji <angel_tommy@foxmail.com>
2021-02-10 06:02:47 +0800

    Update copyright notice

--- init.lua
+++ init.lua
@@ -1,15 +1,21 @@
--- Copyright 2020-2021 Mingming Cui
--- This file is part of Cyberpunk 2077 Open Sesame Mod.
--- Cyberpunk 2077 Open Sesame Mod is free software: you can redistribute it and/or modify
+-- CP77 Open Sesame Mod is a mod which gives you abilities to open
+-- locked doors and vehicles in Cyberpunk 2077.
+
+-- Copyright (C) 2020-2021 Mingming Cui
+--
+-- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
--- Cyberpunk 2077 Open Sesame Mod is distributed in the hope that it will be useful,
+--
+-- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
+--
 -- You should have received a copy of the GNU General Public License
--- along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 registerForEvent("onInit", function()
 	drawPopup = false
